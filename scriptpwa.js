bcc081216bf36f6822ed68339cb81346590852be
4lex8lue <ab.80x08@gmail.com>
2024-11-21 21:10:48 +0600

    rg

--- scriptpwa.js
+++ scriptpwa.js
@@ -1,1 +1,13 @@
 
+if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+                navigator.serviceWorker.register('service-worker.js')
+                            .then(function() {
+                                            console.log('Service Worker registered successfully');
+                                                        })
+                                                                    .catch(function(error) {
+                                                                                    console.error('Service Worker registration failed:', error);
+                                                                                                });
+                                                                                                    });
+                                                                                                    }
+}
